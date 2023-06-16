5599b28bab2233c5f1b6756570e272925038c4b1
JUST-PIX3L <JUSTP1X3L@outlook.com>
2023-06-16 15:19:08 +0100

    ch5 returned model data to website added basic styling

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
+from courses.models import *
 
 def homepage_view(request):
     return render(request, 'homepage.html', {})
 
-def lecturers_view(request):
-    return render(request, 'lecturers.html', {})
-
 def students_view(request):
-    return render(request, 'students.html', {})
+
+    # Return Sensor additional details from DeviceInfo Model
+    student_list = Student.objects.all()
+
+    # Set context and page variables for returning data
+    context = {"student_list": student_list}
+    page_url = "students.html"
+
+    # Return sensor info to html page
+    return render(request, page_url, context)
+
+
+
+def lecturers_view(request):
+
+    # Return Sensor additional details from DeviceInfo Model
+    lecturer_list = Lecturer.objects.all()
+
+    # Set context and page variables for returning data
+    context = {"lecturer_list": lecturer_list}
+    page_url = "lecturers.html"
+
+    # Return sensor info to html page
+    return render(request, page_url, context)
+
+
 
 def courses_view(request):
-    return render(request, 'courses.html', {})
+
+    # Return Sensor additional details from DeviceInfo Model
+    course_list = Course.objects.all()
+
+    # Set context and page variables for returning data
+    context = {"course_list": course_list}
+    page_url = "courses.html"
+
+    # Return sensor info to html page
+    return render(request, page_url, context)
