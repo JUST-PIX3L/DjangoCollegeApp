106a6d53d864adb5870dff86ff296cd1505394ac
JUST-PIX3L <JUSTP1X3L@outlook.com>
2023-06-16 14:49:06 +0100

    ch2 other basic webpages added

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,9 @@ from courses import views
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage'),
+    path('templates/homepage.html', views.homepage_view, name='homepage.html'),
+
+    path('templates/students.html', views.students_view, name='students.html'),
+    path('templates/lecturers.html', views.lecturers_view, name='lecturers.html'),
+    path('templates/courses.html', views.courses_view, name='courses.html'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def homepage_view(request):
     return render(request, 'homepage.html', {})
+
+def lecturers_view(request):
+    return render(request, 'lecturers.html', {})
+
+def students_view(request):
+    return render(request, 'students.html', {})
+
+def courses_view(request):
+    return render(request, 'courses.html', {})
